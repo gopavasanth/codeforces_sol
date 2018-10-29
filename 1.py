3f74f9f39273c9ab5d8b55c60b8521ec3d4faeb1
SriAlavandar <40352419+SriAlavandar@users.noreply.github.com>
2018-10-29 17:03:19 +0530

    Hacktoberfest

--- 1.py
+++ 1.py
@@ -3,12 +3,12 @@ b=[]
 c=0
 sum=0
 for i in range (n):
-    b[i] = int(input())
+    b[i] = int(input())//Inputing the values.
 
 for j in range (n):
     for k in range (n):
         if b[j]==b[k]:
-            c++
+            c++//Incrementing this value
     if c>2:
         l.append[]
     

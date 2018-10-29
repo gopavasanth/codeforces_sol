98f1664b62d04a93328495fac66e0d8fb385fcaf
Athi Dsh <32543864+Athi101@users.noreply.github.com>
2018-10-29 11:05:13 +0530

    Update 118A.py

--- 118A.py
+++ 118A.py
@@ -1,11 +1,11 @@
+#This takes input
 word = input()
 word = word.lower()
 l = []
+#this is for loop 
 for i in range (len(word)):
     if i == 'a' or i=='e' or i == 'i' or i == 'o' or i == 'u' or i=='y' :
         True
     else:
         l.append('.'+word[i])
-
 print("".join(l))        
-    

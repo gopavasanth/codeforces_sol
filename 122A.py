65737fc9f0bb4fe390bff2ffbb5a300a67cd6367
kvsnagasai <33051967+kvsnagasai@users.noreply.github.com>
2019-10-27 15:47:13 +0530

    Update 122A.py

--- 122A.py
+++ 122A.py
@@ -1,8 +1,8 @@
-n = int(input())
+n=int(input())
 for i in range (len(n)):
-    if i == 7 or i == 4:
-        print ("YES")
-if (n%7 == 0 or n%4 == 0):
+    if (i == 7 or i == 4):
+        print("YES")
+if ( n%7 == 0 or n%4==0):
     print ("YES")
 else :
     print ("NO")

cb55f3da7458a94db6e835b1d9a2c6baed9f4f5f
kvsnagasai <33051967+kvsnagasai@users.noreply.github.com>
2019-10-27 15:45:48 +0530

    Update 1004A.py

--- 1004A.py
+++ 1004A.py
@@ -1,11 +1,11 @@
 a=list(map(int,input().split()))
 t=a[0]
 d=a[1]
-arr=list(map(int,input().split()))
+b=list(map(int,input().split()))
 count=0
 for i in range(t-1):
-    if(arr[i+1]-arr[i]==2*d):
+    if(b[i+1]-b[i]==2*d):
         count+=1
-    elif(arr[i+1]-arr[i]>2*d):
+    elif(b[i+1]-b[i]>2*d):
         count+=2
 print(count+2)

be2cb7ca447d1b0dd5376edb447e5e4c9f7aef05
Athi101 <aathira2017@gmail.com>
2021-10-03 18:50:02 +0530

    Modified: reduce two nested loops

--- 1140B.py
+++ 1140B.py
@@ -5,15 +5,11 @@ rep = int(input())
 for i in range(rep):
     a = int(input())
     string = input()
-
+    count = 0
     if(string[0] == "<" and string[-1] == ">"):
-        count = 0
         for i in range(len(string)):
             if(string[i] == "<" and string[len(string)-1-i] =='>' ):
                 count += 1
             else:
                 break
-        print(count)
-
-    else:
-        print(0)
+    print(count)

c5872a8b87e3944008abe0b05928a28844b7eae1
bysanishanmuga <73063406+bysanishanmuga@users.noreply.github.com>
2021-10-03 11:49:20 +0530

    Update 1312A_Two_Regular_Polygons.cpp

--- 1312A_Two_Regular_Polygons.cpp
+++ 1312A_Two_Regular_Polygons.cpp
@@ -3,18 +3,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     int t;
     cin>>t;
-    while (t--)
-    {
+    while (t--) {
         int n, m;
         cin >> n >> m;
-        if (n % m == 0)
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
+        if (n % m == 0) {
+            cout << "YES" << endl;        
+        }
+        else {
+            cout << "NO" << endl;        
+        }
     }
     return 0;
 }

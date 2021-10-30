0a3c63fec36a55a581bb511b2eb2bde180811442
Mani__Idamakanti_ <35557678+Maniidamakanti28@users.noreply.github.com>
2021-10-30 15:11:35 +0530

    Update 110A.py

--- 110A.py
+++ 110A.py
@@ -1,9 +1,9 @@
 #Question: https://codeforces.com/problemset/problem/110/A 
 
-n = input()
+num = input()
 count = 0
-for i in range(len(n)):
-    if (n[i]) == '4' or (n[i]) == '7':
+for i in range(len(num)):
+    if (num[i]) == '4' or (num[i]) == '7':
         count = count+1       
 if count == 7 or count == 4  :
     print("YES")
